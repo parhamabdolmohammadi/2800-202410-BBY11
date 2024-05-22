fb33a36d50a84ea321572562f4e66f919b83e496
Parham Abdolmohammadi <parham.abdo@gmail.com>
2024-05-21 23:26:20 -0700

    Extracting service name from local storage and setting up the station.ejs form based on it

--- scripts/station.js
+++ scripts/station.js
@@ -7,4 +7,10 @@ function fillCard(stat) {
    document.getElementById('card-availability').innerHTML = stat.robots_available;
    document.getElementById('card-phone').innerHTML = stat.contact_number;
    document.getElementById('station-distance').innerHTML = Math.floor(distance1 * 100) / 100 + " km" ;
+
+  
+let serviceName = localStorage.getItem('ServiceName');
+   document.getElementById('Selected-service').innerHTML =serviceName;
+
+
 }
