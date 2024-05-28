9fe167becba153bb2e82db46f20aac4adb2be054
Coda Miles <milescoda@gmail.com>
2024-05-27 22:32:29 -0700

    added order details to database and created uuid for order number

--- scripts/confirmation.js
+++ scripts/confirmation.js
@@ -3,5 +3,11 @@ let price = localStorage.getItem('ServicePrice');
 let total = price*1.12;
 total = parseFloat(total.toFixed(2));
 
+const url = new URL(window.location.href);
+url.searchParams.append("total", total);
+
+url.searchParams.append("service", service);
+window.history.pushState({}, '', url);
+
 document.getElementById('service').innerHTML = "Robo Service: "+service;
 document.getElementById('price').innerHTML = "Price: $"+total;
