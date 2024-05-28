4a445bbaacf272323b4b2eb3bd16f59c5ba37499
Parham Abdolmohammadi <parham.abdo@gmail.com>
2024-05-28 14:58:38 -0700

    businessChekout form is provided for businessOwners to add robots

--- scripts/businessCheckout.js
+++ scripts/businessCheckout.js
@@ -4,8 +4,9 @@ fillCard(station);
 function fillCard(stat) {
    document.getElementById('card-address').innerHTML = stat.address;
    document.getElementById('station-name').innerHTML = stat.station_name;
-   document.getElementById('card-availability').innerHTML = stat.robots_available;
-   document.getElementById('card-phone').innerHTML = stat.contact_number;
+   // for Coda current card Availability
+//    document.getElementById('card-availability').innerHTML = stat.robots_available;
+//    document.getElementById('card-phone').innerHTML = stat.contact_number;
    document.getElementById('station-distance').innerHTML = Math.floor(distance1 * 100) / 100 + " km" ;
 
    localStorage.setItem('StationID', stat._id);
@@ -13,29 +14,18 @@ function fillCard(stat) {
 let serviceName = localStorage.getItem('ServiceName');
    document.getElementById('Selected-service').innerHTML =serviceName;
    let price = localStorage.getItem('ServicePrice');   
-   document.getElementById('price').innerHTML ='$'+price;
+//    document.getElementById('price').innerHTML ='$'+price;
 
 
     document.querySelector('.id-placeholder2').id =  "id-placeholder2" +IDCard  ;
 
     document.querySelector('.id-placeholder11').id =  "id-placeholder11" + distance1;
 
-    document.querySelector('.button1').onclick  =  () => setInputs();
-
-
+    setInputs();
 }
 
-
-function setInputs() {
-   document.getElementById("id-placeholder2" + IDCard ).value = IDCard;
-   document.getElementById("id-placeholder11" + distance1).value = distance1;
-   document.querySelector('.id-button2').click();
-
- 
-}
-
-
-
-
-
-
+// if you want to transfer data to the next page here is a good way just copy the form in the station.ejs here and use it
+// function setInputs() {
+//    document.getElementById("id-placeholder2" + IDCard ).value = IDCard;
+//    document.getElementById("id-placeholder11" + distance1).value = distance1;
+// }
